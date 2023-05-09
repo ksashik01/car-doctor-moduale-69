795c20ef8a295c9c0198fda2a0ff0679d6206c37
ksashik01 <complainsheet@gmail.com>
2023-05-10 01:57:33 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from "react-dom/client";
+
+
+import './index.css'
+
+import {
+ 
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <div className='max-w-7xl mx-auto'>
+  <React.StrictMode>
+      
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+  </React.StrictMode>,
+
+  </div>
+)
